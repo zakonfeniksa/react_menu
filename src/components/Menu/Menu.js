6cbf9fdece1ff10ce32be58f9195cac09942adaf
Stas Miotk <tgb.stas@hotmail.com>
2021-01-09 20:58:23 +0100

    Add & export component Menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,2 +1,21 @@
 import React, { Component } from 'react';
 import './Menu.css';
+
+class Menu extends Component{
+  render(){
+    let visibility = "hide";
+
+    if(this.props.menuVisibility){
+      visibility = "show";
+    }
+    return(
+      <div id="flyoutMenu" onMouseDown={this.props.handleMouseDown} className={visibility}>
+        <h2><a href="#">Strona główna</a></h2>
+        <h2><a href="#">O nas</a></h2>
+        <h2><a href="#">Kontakt</a></h2>
+        <h2><a href="#">Szukaj</a></h2>
+    );
+  }
+}
+
+export default Menu;
