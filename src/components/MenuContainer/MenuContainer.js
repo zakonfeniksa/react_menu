3647d02c41669912dca7ba699a2b23e1e2203a71
Stas Miotk <tgb.stas@hotmail.com>
2021-01-09 20:27:15 +0100

    Add & export component MenuContainer

--- src/components/MenuContainer/MenuContainer.js
+++ src/components/MenuContainer/MenuContainer.js
@@ -1,2 +1,25 @@
 import React, { Component } from 'react';
 import './MenuContainer.css';
+
+class MenuContainer extends Component {
+  render(){
+    return(
+      <div>
+        <div>
+          <p>Który z poniższych elementów nie pasuje do pozostałych?</p>
+          <ul>
+            <li>Drzewo</li>
+            <li>Trzcina</li>
+            <li>Ryba</li>
+            <li>Las</li>
+            <li>Trawa</li>
+            <li>Róża</li>
+            <li>Pomidor</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MenuContainer;
